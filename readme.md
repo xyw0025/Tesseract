2834640baab28afcbb1d4073f728df4934dc3845
xyw0025 <xyw0025@gmail.com>
2020-09-10 20:07:31 +0800

    init

---
+++ readme.md
@@ -0,0 +1,17 @@
+The file `chi_tra_TRAIN.traineddata` should be put under path /usr/local/share/tessdata/ , etc.  
+If it's installed succesfully, chi_tra_TRAIN can be seen using command "tesseract --list-langs"  
+
+
+### to run:  
+
+python testTesseract.py -i [input_files_folder_path] -f [output_format(xml, txt)]
+	
+```
+ex: 
+	python testTesseract.py -i input_data_example/ -f xml
+```
+
+It will iterate over files under specified folder
+By default, language is chosen as chi_tra_TRAIN, the output files will be placed in ./output
+
+
---
+++ tesseract.py
@@ -0,0 +1,43 @@
+import pytesseract
+from argparse import ArgumentParser 
+import xml.etree.cElementTree as ET
+import os
+
+output_path = "./output/"
+config="./config_set"
+# What the file config_set does here is to remove all the spaces between every character in the output
+
+def read_image_to_data(folder_path, filename, formt="xml", lang="chi_tra_TRAIN"):
+	output_name = filename.split('.')[0]
+	if formt == "xml":
+		output_name += ".xml"
+		output_file = pytesseract.image_to_alto_xml(folder_path, lang=lang)
+		with open(output_path + output_name, 'w+b') as f:
+			f.write(output_file)
+	if formt == "txt":
+		output_name += ".txt"
+		output_file = pytesseract.image_to_string(folder_path, lang=lang, config=config)
+		with open(output_path + output_name, 'w') as f:
+			f.write(output_file)
+	else:
+		print("error: output format as .xml or .txt")
+		return
+	print("-" *5 + filename, "reading images to", format ) 
+
+
+
+if __name__ == '__main__':
+	
+	parser = ArgumentParser()
+	parser.add_argument("-i", "--input_file", dest="input_path")
+	parser.add_argument("-f", "--format", dest="format")
+	args = parser.parse_args()
+	path = args.input_path
+	format = "txt"
+	if args.format != None:
+		format = args.format
+
+	for file in os.listdir(path):
+		filename = os.fsdecode(file)
+		if filename.endswith(".png") or filename.endswith(".jpg"):
+			read_image_to_data(path+filename, filename, format)
